f9d5754afc3103b3d5d2db6b44432d07c56c6b96
jnvillalba <joaquinvillalba8@gmail.com>
2022-07-04 18:39:26 -0300

    Creo Proyecto

---
+++ rock-paper-scissors-lizard-spock/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import {useEffect, useState} from 'react';
+
+function App() {
+  return(
+    
+  )
+
+}
+
+export default App;
+
+/*
+*Player
+-Elegir jugada
+-ver puntuacion
+*Computer
+-elegir random mano
+*/
