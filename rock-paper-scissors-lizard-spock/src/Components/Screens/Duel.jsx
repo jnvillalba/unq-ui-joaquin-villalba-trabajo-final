e3afc3267fc6a8d255f4a56b58ccbccc4f5a0cd7
jnvillalba <joaquinvillalba8@gmail.com>
2022-07-12 03:28:41 -0300

    borro cosas q no se usan

--- rock-paper-scissors-lizard-spock/src/Components/Screens/Duel.jsx
+++ rock-paper-scissors-lizard-spock/src/Components/Screens/Duel.jsx
@@ -1,20 +1,17 @@
-import React, {useState } from 'react'
+import React from 'react'
 import './Duel.css'
-import { handsList } from '../Hands/handsList'
+
 
 const Duel = ({playerOption, botOption, winner}) => {
-  const [showWinner, setShowWinner] = useState(true);
+
 
   return (
     <div className='box-duel'>
       <div className='definitive-option'>{playerOption}</div>
       {<div className="winner-text">{winner} </div>}
-      <div className='vs'>VS
-
-      </div>
+      <div className='vs'>VS</div>
       <div className='definitive-option'>{botOption}</div>
     </div>
-
   )
 }
 
